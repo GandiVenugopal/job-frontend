84825233e9140d6984fdd2e7238caea7ee3d3e22
GandiVenugopal <venugopalgandi007@gmail.com>
2025-03-28 00:30:00 -0400

    Use env variable for backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://18.191.212.115:5000/api',
+  baseURL: process.env.REACT_APP_BACKEND_URL+'/api',
 });
 
 // Attach token if exists
